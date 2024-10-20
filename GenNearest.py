dd1b7dcc193dd4e4bc21b228ddf192c1f7b5341f
Emilien <emilien.komlenovic@gmail.com>
2024-10-20 17:49:32 +0200

    Initial commit

---
+++ GenNearest.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import cv2
+import os
+import pickle
+import sys
+import math
+
+from VideoSkeleton import VideoSkeleton
+from VideoReader import VideoReader
+from Skeleton import Skeleton
+
+
+
+class GenNeirest:
+    """ class that Generate a new image from videoSke from a new skeleton posture
+       Fonc generator(Skeleton)->Image
+       Neirest neighbor method: it select the image in videoSke that has the skeleton closest to the skeleton
+    """
+    def __init__(self, videoSkeTgt):
+        self.videoSkeletonTarget = videoSkeTgt
+
+    def generate(self, ske):           
+        """ generator of image from skeleton """
+        empty = np.ones((64,64, 3), dtype=np.uint8)
+        distance = 10000
+        image = empty
+        for i in range(self.videoSkeletonTarget.skeCount()):
+            skeTgt = self.videoSkeletonTarget.ske[i]
+            if skeTgt.distance(ske) < distance:
+                distance = skeTgt.distance(ske)
+                image = self.videoSkeletonTarget.readImage(i)
+        return image
+
+
+
+
